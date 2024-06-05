0d2916ae62ff2766bf160669ea628a5b8fea4191
Konsta <64072163+konstaz@users.noreply.github.com>
2024-06-05 22:43:04 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+1.pip install -r requirements.txt
+2.python main.py 
+
+P.S. You can also add path to the specific files and the command should look like this -> 
+python main.py python main.py --mask mask.png --color_mask color_mask.png 
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
+import argparse
 import cv2
 import numpy as np
 from collections import Counter
 
 
 class ImagePainter:
-    def __init__(self, polygons_img='mask.png', colored_img='color_mask.png'):
-        self.polygons_img = cv2.imread(polygons_img)
-        self.colored_img = cv2.imread(colored_img)
-        self.gray_polygons = cv2.cvtColor(self.polygons_img, cv2.COLOR_BGR2GRAY)
-        self.contours, _ = cv2.findContours(self.gray_polygons, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def __init__(self, mask='mask.png', color_mask='color_mask.png'):
+        self.mask = cv2.imread(mask)
+        self.color_mask = cv2.imread(color_mask)
+        self.gray_background = cv2.cvtColor(self.mask, cv2.COLOR_BGR2GRAY)
+        self.contours, _ = cv2.findContours(self.gray_background, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     def process_data(self):
-        result_img = np.zeros_like(self.colored_img)
-        contours_img = np.zeros_like(self.colored_img)
+        result_img = np.zeros_like(self.color_mask)
+        contours_img = np.zeros_like(self.color_mask)
         cv2.drawContours(contours_img, self.contours, -1, (0, 0, 0), 1)
 
         for contour in self.contours:
-            mask = np.zeros_like(self.gray_polygons)
+            mask = np.zeros_like(self.gray_background)
             cv2.drawContours(mask, [contour], -1, (255, 255, 255), -1)
 
-            pixels = self.colored_img[mask == 255]
+            pixels = self.color_mask[mask == 255]
 
             if len(pixels) > 0:
                 most_common_color = Counter(map(tuple, pixels)).most_common(1)[0][0]
@@ -32,5 +33,15 @@ class ImagePainter:
 
 
 if __name__ == '__main__':
-    image = ImagePainter()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mask')
+    parser.add_argument('--color_mask')
+    args = parser.parse_args()
+    if args.mask is not None and args.color_mask is not None:
+        args = parser.parse_args()
+        image = ImagePainter(args.mask, args.color_mask)
+    else:
+        image = ImagePainter()
     image.process_data()
+
+
